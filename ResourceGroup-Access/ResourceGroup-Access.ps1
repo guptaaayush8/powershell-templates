0f2263b99bb5336e37a0dec4c3bc58929d6914ff
Aayush Gupta <aayush.g1@tcs.com>
2021-03-31 17:33:41 +0530

    script verbosed

--- ResourceGroup-Access/ResourceGroup-Access.ps1
+++ ResourceGroup-Access/ResourceGroup-Access.ps1
@@ -1,20 +1,31 @@
 ﻿param(
     $ResourceGroupName,
+    $Location = "East US",
     $Role=@('Contributor'),
     $GroupID = @('25c3d51c-622d-43c6-9fde-b437dd1e2222')
 )
 
+$ResourceExist = Get-AzResourceGroup -Name "$ResourceGroup"  -ErrorAction SilentlyContinue
 
 if(-not $ResourceExist){
-    New-AzResourceGroup -Name $ResourceGroupName -Location "East US"
+    Write-Host "resource group '$ResourceGroupName' does not exist, Creating it"
+    try{
+        New-AzResourceGroup -Name $ResourceGroupName -Location $Location
+        Write-Host "resource group created"
+    }
+    catch{
+       throw $_.exception 
+    }
+
 }else{
-    write-host "Resource Already Exist"
+    write-host "Resource Group Already Exist"
 }
 
 try{
+    Write-Host "Attempting to give role - '$Role' to group id - '$GroupID'"
     New-AzRoleAssignment -ObjectId $GroupID -RoleDefinitionName $Role -ResourceGroupName $ResourceGroupName
 }
 catch{
-    $_   
+    Write-Host "Role already exist"   
 }
 
