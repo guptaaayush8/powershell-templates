f24b3d2ec8219222ffd86c7e35b04fd8ee452057
Aayush Gupta <aayush.g1@tcs.com>
2021-03-31 17:13:14 +0530

    resourcegroup-access

---
+++ ResourceGroup-Access/ResourceGroup-Access.ps1
@@ -0,0 +1,20 @@
+﻿param(
+    $ResourceGroupName,
+    $Role=@('Contributor'),
+    $GroupID = @('25c3d51c-622d-43c6-9fde-b437dd1e2222')
+)
+
+
+if(-not $ResourceExist){
+    New-AzResourceGroup -Name $ResourceGroupName -Location "East US"
+}else{
+    write-host "Resource Already Exist"
+}
+
+try{
+    New-AzRoleAssignment -ObjectId $GroupID -RoleDefinitionName "Contributor" -ResourceGroupName $ResourceGroupName
+}
+catch{
+    
+}
+
