f495a78952bf9c083d836230b6a956119699853c
Aayush Gupta <aayush.g1@tcs.com>
2021-04-08 15:25:33 +0530

    Added script to check valid jsons in repo

---
+++ CheckValid-Json/CheckValid-Json.ps1
@@ -0,0 +1,24 @@
+﻿$jsonFiles = Get-ChildItem -Path (Get-Location).Path -Filter "*.json" -Recurse
+
+$InvalidTemplate = @()
+
+foreach($jsonFile in $jsonFiles){
+    try{
+        $null = Get-Content -Path $jsonFile.FullName | Convertfrom-Json
+    }
+    catch{
+        $InvalidTemplate += " $($JsonFile.Name) `n $($_.Exception)" 
+    }
+}
+
+if($InvalidTemplate.Count -ne 0){
+    
+    throw "$InvalidTemplate"
+    
+}
+
+else {
+    "All JSONs are valid"
+
+}
+
