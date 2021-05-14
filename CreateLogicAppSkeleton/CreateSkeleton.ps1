6627d8071c377f5b41254ce821a2dc9d90580106
Aayush Gupta <aayush.g1@tcs.com>
2021-05-14 19:34:54 +0530

    Added Skeleton Fetching Script

---
+++ CreateLogicAppSkeleton/CreateSkeleton.ps1
@@ -0,0 +1,81 @@
+﻿Connect-AzAccount -Subscription lyb-nonprd
+$rg = 'rg-integration-svccsvtoexcel-dev'
+$app = 'lapp-svccsvtoexcel'
+$VerbosePreference = 'Continue'
+
+$logicapp = Get-AzResource -ResourceGroupName $RG -ResourceType Microsoft.Logic/workflows -ResourceName "$App"
+$logicapp.properties.definition | ConvertTo-Json
+
+$def = $logicapp.Properties.definition
+
+$actions = $def.actions
+
+class Node{
+    [String]$Name
+    [Node[]]$NextNodes
+    [Node]$SubTree
+    [String[]]$ParentStatus
+}
+
+
+#FindParent
+function FindParentNode{
+param($actions)
+    
+    $LayerItemsP = ($actions|gm -MemberType NoteProperty).Name
+    foreach($item in $LayerItemsP){
+
+        if(($actions.$item.runAfter|gm -MemberType NoteProperty).count -eq 0){
+            $NodeObj = New-Object Node -Property @{Name = $item; ParentStatus ='Initial'}
+            return $NodeObj
+        }
+    }
+
+}
+
+function FindNextNodes{
+    param($action,$CurrentNode)
+    $Name  = $currentnode.name
+    $LayerItems = ($actions|gm -MemberType NoteProperty).Name|where {$_ -ne $Name}
+    $Nodes =  @()
+    foreach($item in $LayerItems){
+        if(($action.$item.runAfter|select $Name).$Name){
+            $Nodes += New-Object Node -Property @{Name = $item; Parentstatus = $action.$item.runAfter.$Name }
+        }
+    }
+    return $Nodes
+}
+
+function RecurseAttach{
+    param($actions,$Parent)
+    $NextNodes = FindNextNodes -action $actions -CurrentNode $Parent
+    $parent.NextNodes = $NextNodes
+    
+    Write-Verbose ($Parent|Out-String)
+    if($parent.NextNodes -eq $null){return $null}
+
+    foreach($node in $parent.NextNodes){
+        $NodeName = $node.Name
+        if(($actions.$NodeName|gm -MemberType NoteProperty).Name -contains 'actions'){
+            
+            $InternalParent = FindParentNode -actions $actions.$NodeName.actions
+            $InternalTree = RecurseAttach -actions $actions.$NodeName.actions -Parent $InternalParent
+            $($parent.NextNodes|where{$_.name -eq $NodeName}).SubTree = $InternalTree
+        }
+        try{
+            $parent.NextNodes.nextNodes = RecurseAttach -actions $actions -Parent $node 
+        }
+        catch{}
+    }
+    return $Parent
+}
+
+
+$root = FindParentNode -actions $actions
+
+$LogicAppStructure = RecurseAttach -actions $actions -Parent $root
+
+
+$LatestRun = Get-AzLogicAppRunHistory -ResourceGroupName $rg -Name $app |select -First 1
+$TriggerAction = Get-AzLogicAppTriggerHistory -ResourceGroupName $rg -Name $app -HistoryName $LatestRun.Name -TriggerName $LatestRun.Trigger.Name -Verbose
+$RunAction = (Get-AzLogicAppRunAction -ResourceGroupName "$RG" -Name "$App" -RunName $LatestRun.Name)|sort -Property "StartTime","EndTime"|select 'Name','Status','Code','StartTime','EndTime'
--- RestAPI/CheckFloating Branches.ps1
+++ RestAPI/CheckFloating Branches.ps1
@@ -8,15 +8,19 @@ $repoBody = @{
     name = "TestRepo"
 } | ConvertTo-Json
 
-$Repo = Invoke-RestMethod -method Post -Headers $AzureDevOpsAuthenicationHeader -body $repoBody -Uri $projUrl -ContentType "application/json" -UseBasicParsing
+#$Repo = Invoke-RestMethod -method Post -Headers $AzureDevOpsAuthenicationHeader -body $repoBody -Uri $projUrl -ContentType "application/json" -UseBasicParsing
 $Repo = Invoke-RestMethod -Headers $AzureDevOpsAuthenicationHeader -Uri $projUrl
-
+$Branches = @()
 foreach($r in $repo.value){
     $uri = "https://dev.azure.com/LyondellBasell/Cloud Integration/_apis/git/repositories/$($r.id)/refs?api-version=6.0"
-$p = Invoke-RestMethod -Headers $AzureDevOpsAuthenicationHeader -Uri $uri
-"-------------$($r.name)"
-$p.value.name 
+    $p = Invoke-RestMethod -Headers $AzureDevOpsAuthenicationHeader -Uri $uri
+    
+    foreach($i in $p.value){
+        
+        
+        $Branches += [pscustomobject]@{Repo = $R.name; Branch = $i.name; Creator = $i.creator.displayName}
+    }
 }
-
+$Branches|sort -Property repo
 
 $projUrl = "https://dev.azure.com/LyondellBasell/_apis/distributedtask/yamlschema?api-version=5.1"
