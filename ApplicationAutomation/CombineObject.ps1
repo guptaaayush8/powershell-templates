8ef22c31f264907e7c930c3dbe332d503fcf0076
Aayush Gupta <aayush.g1@tcs.com>
2021-05-29 16:02:40 +0530

    Added Combine Object Script

---
+++ ApplicationAutomation/CombineObject.ps1
@@ -0,0 +1,19 @@
+﻿
+function CombineObject{
+    param(
+        $Base,
+        $Additive
+    )
+
+    $AdditiveMembers = ($Additive|gm -MemberType NoteProperty).Name
+    $BaseMembers = ($Base|gm -MemberType NoteProperty).Name
+    foreach($AdditiveMember in $AdditiveMembers){
+        if($BaseMembers -notcontains $AdditiveMember){
+            $Base|Add-Member -MemberType NoteProperty -Name $AdditiveMember -Value $Additive.$AdditiveMember -Force
+        }
+        else{
+           $Base|Add-Member -MemberType NoteProperty -Name $AdditiveMember -Value (CombineObject -Base $Base.$AdditiveMember -Additive $Additive.$AdditiveMember) -Force
+        }
+    }
+    return $Base
+}
