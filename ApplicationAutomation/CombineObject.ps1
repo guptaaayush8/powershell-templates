1a40ffd23c0eee60d3df8990381be6db499973b3
Aayush Gupta <aayush.g1@tcs.com>
2021-06-02 14:16:06 +0530

    Updated Script to combine object

--- ApplicationAutomation/CombineObject.ps1
+++ ApplicationAutomation/CombineObject.ps1
@@ -1,19 +1,108 @@
 ﻿
-function CombineObject{
+$Base = gc .\CommonARM.json|ConvertFrom-Json
+$Additive = gc .\resources\file.json|ConvertFrom-Json
+function CombineParameters{
     param(
         $Base,
         $Additive
     )
+    foreach($param in ($additive.parameters|gm -MemberType NoteProperty).Name){
+    try{
+        $Base.parameters|Add-Member -MemberType NoteProperty -Name $param -Value $Additive.parameters.$param -Force
+    }catch{}
+    }
+    return $Base.parameters|select -Unique
+}
+function CombineVariables{
+    param(
+        $Base,
+        $Additive
+    )
+    foreach($Var in ($additive.variables|gm -MemberType NoteProperty).Name){
+    try{
+        $Base.variables|Add-Member -MemberType NoteProperty -Name $Var -Value $Additive.variables.$Var -Force
+    }catch{}
+    }
+    return $Base.variables|select -Unique
+}
+function CombineOutputs{
+    param(
+        $Base,
+        $Additive
+    )
+    try{
+    foreach($Var in ($additive.Outputs|gm -MemberType NoteProperty -ErrorAction SilentlyContinue).Name){
+    try{
+        $Base.outputs|Add-Member -MemberType NoteProperty -Name $Var -Value $Additive.outputs.$Var -Force
+    }catch{}
+    }}catch{}
+    return $Base.outputs|select -Unique
+}
 
-    $AdditiveMembers = ($Additive|gm -MemberType NoteProperty).Name
-    $BaseMembers = ($Base|gm -MemberType NoteProperty).Name
-    foreach($AdditiveMember in $AdditiveMembers){
-        if($BaseMembers -notcontains $AdditiveMember){
-            $Base|Add-Member -MemberType NoteProperty -Name $AdditiveMember -Value $Additive.$AdditiveMember -Force
+
+
+
+function CombineResources{
+    param($Base,$Additive)
+
+    $BaseNames = $Base.resources.name
+    $AdditiveNames = $Additive.resources.name
+    $ResourceObject =@()
+    foreach($Name in $AdditiveNames){
+        if($BaseNames -contains $Name){
+            Write-Verbose $Name
+            $AddObject = $Additive.resources|where{$_.name -eq $Name}
+            $BaseObject = $Base.resources|where{$_.name -eq $Name}
+            $ResourceObject += (CombineObject -BaseObject $BaseObject -AddObject $AddObject)
         }
         else{
-           $Base|Add-Member -MemberType NoteProperty -Name $AdditiveMember -Value (CombineObject -Base $Base.$AdditiveMember -Additive $Additive.$AdditiveMember) -Force
+        Write-Verbose "Else"
+        Write-Verbose $Name
+            $ResourceObject += $Additive.resources|where{$_.name -eq $Name}
         }
     }
-    return $Base
+
+
+    foreach($Name in $BaseNames){
+        if($ResourceObject.Name -notcontains $Name){
+        $ResourceObject += $Base.resources|where{$_.name -eq $Name}
+        }
+    }
+    return $ResourceObject
 }
+
+
+function CombineObject{
+    param(
+        $BaseObject,$AddObject
+    )
+    Write-Verbose ($BaseObject|Out-String)
+    Write-Verbose ($AddObject|Out-String)
+    $BaseMembers = ($BaseObject|gm -MemberType NoteProperty).Name
+    $AddMembers = ($AddObject|gm -MemberType NoteProperty).Name
+   # $Obj = New-Object psobject
+    foreach($Member in $AddMembers){
+        if($BaseMembers -notcontains $Member){
+            $BaseObject|Add-Member -MemberType NoteProperty -Name $Member -Value $AddObject.$Member
+        }
+        else{
+
+            if($Baseobject.$Member.GetType().Name -eq 'String' -or $Baseobject.$Member.GetType().Name -match 'Object\[\]'){
+                $BaseObject.$Member =  ($BaseObject.$Member,$AddObject.$Member|select -Unique)
+            }
+            else{
+                $BaseObject.$Member =  (CombineObject $BaseObject.$Member $AddObject.$Member)
+            }
+        }
+    }
+    return $BaseObject
+}
+
+
+
+
+$base.variables = CombineVariables -Base $base -Additive $additive
+$base.parameters = CombineParameters -Base $base -Additive $additive
+$base.outputs = CombineOutputs -Base $base -Additive $additive
+#$base.resources =
+CombineResources -Base $Base -Additive $Additive
