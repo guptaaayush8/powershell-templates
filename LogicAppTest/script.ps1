529dd16a3110e06d6db88cf0ce3b22351948e7fa
Aayush Gupta <aayush.g1@tcs.com>
2021-06-26 22:38:03 +0530

    Deployment Name Changed

---
+++ LogicAppTest/script.ps1
@@ -0,0 +1,26 @@
+﻿#Deployment Check 
+
+$RG = 'rg-integration-excisetax813-dev' ## $(rg-dev)
+
+
+$Collection = ([xml](gc .\DeploymentCollection.xml)).Collection.Resources
+
+
+
+$resource = Get-AzResource -ResourceGroupName $Collection.ResourceGroup  -Name $collection.Resource.Name -ResourceType $Collection.Resource.ResourceType
+
+$Queue = Get-AzServiceBusQueue -ResourceGroupName rg-integration-systemapp-dev -Namespace sb-integration-lyb-dev
+
+$Topics = Get-AzServiceBusTopic -ResourceGroupName rg-integration-systemapp-dev -Namespace sb-integration-lyb-dev 
+
+$Subs = Get-AzServiceBusSubscription -ResourceGroupName rg-integration-systemapp-dev -Namespace sb-integration-lyb-dev -Topic sbt-cmn-publish
+
+
+
+
+
+
+
+
+
+
